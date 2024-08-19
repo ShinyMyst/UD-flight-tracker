9dc32ca67957e039f0e3b72387857dfb1a003931
ShinyMyst <122401790+ShinyMyst@users.noreply.github.com>
2024-08-19 15:15:56 -0400

    Email reminders functional
    Script still requires some tweak but it works as intended.

--- main.js
+++ main.js
@@ -1,96 +1,83 @@
 function main() {
-  // Get the active spreadsheet
+  // Prepare Tabs
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  
-  // Define the tab names using variables
-  var emailTabName = 'Control'; 
   var tasksTabName = 'Tasks'; 
-
-  // Get the sheets (tabs) by their names
+  var emailTabName = 'Control'; 
+  var taskSheet = spreadsheet.getSheetByName(tasksTabName);
   var emailSheet = spreadsheet.getSheetByName(emailTabName);
-  var tasksSheet = spreadsheet.getSheetByName(tasksTabName);
 
   // Set Variables
-  var taskCol = 3 
-  var taskRow = 1
-  var emailCol = 2
-  var emailRow = 2
-  // Email is assumed adjacent
-    // Get the last column with data
-  var lastColumn = tasksSheet.getLastColumn();
+  var taskNameCol = 3 // Col holding name of task
+  var startCol = 4    // First col with an RA name
+  var emailRow = 2    // Row with first email address
+  var emailCol = 2    // Col holding email addresses
+
+  var missingTasks = getMissingTasks(taskSheet, taskNameCol, startCol)
+  sendReminders(emailSheet, emailRow, emailCol, missingTasks);
+}
+
+////////////
+// Functions
+////////////
+function getMissingTasks(taskSheet, taskNameCol, startCol){
+  // Returns a list of lists.  
+  // Each sub-list contains all missing tasks in that column.
+
+  var lastColumn = taskSheet.getLastColumn();
+  var lastRow = taskSheet.getLastRow();
+  var allTasksList = []
   
-  // Get the last row with data
-  var lastRow = tasksSheet.getLastRow();
-
-  // Prep Collection
-  var allTasks = []
-
-  for (var col = 4; col <= lastColumn; col++) { // Change start col as a var
+  // Iterate through each column for missing tasks
+  for (var col = startCol; col <= lastColumn; col++) { 
     // Iterate through each row in the current column
-    //console.log("NEW RA");
-    var raName = tasksSheet.getRange(taskRow, col).getValue();
-    var taskList = [];
+    var colTaskList = [];
     for (var row = 1; row <= lastRow; row++) {
-      // Get the value of the current cell
-      var cellValue = tasksSheet.getRange(row, col).getValue();
-      
-      // Check if the cell value is ❌
+      var cellValue = taskSheet.getRange(row, col).getValue();     
       if (cellValue === '❌') {
-        // Get values from the taskRow and taskCol
-        var taskName = tasksSheet.getRange(row, taskCol).getValue();
-        taskList.push(taskName)
+        var taskName = taskSheet.getRange(row, taskNameCol).getValue();
+        colTaskList.push(taskName)
       }
     }
-  console.log(raName)
-  console.log(taskList)
-  //taskDict[raName] = taskList
-  allTasks.push(taskList)
+    allTasksList.push(colTaskList)
   }
-  console.log(allTasks)
+  return allTasksList;
+}
 
-  // Send the Emails
-    // Get the last row with data in the specified column
+function sendReminders(emailSheet, emailRow, emailCol, tasks) {
+  /* Sends email reminders to RAs that have a missing task */
   var lastRow = emailSheet.getLastRow();
   
   // Get the range of the column from the start row to the last row
   var range = emailSheet.getRange(emailRow, emailCol, lastRow - emailRow + 1); //email row = startRow
-  // Get the values from the range
   var values = range.getValues();
 
   // Iterate through each row and log the value
   for (var i = 0; i < values.length; i++) {
-    var cellValue = values[i][0]; // Get the value in the cell
-    console.log('Email:' + cellValue);
-    console.log(allTasks[i])
-  }
-}
+    var emailAddress = values[i][0]; 
+    if (tasks[i].length > 0){
+      // console.log('Email:' + emailAddress);
+      // console.log('Title: [REMINDER] Missing Tasks')
+      // console.log('You are missing the following tasks.')
+      // console.log(tasks[i])
+      // console.log('Please check your Flight Tracker to get these compelted.')
+      let subject = "[REMINDER] Missing Tasks"
 
-function getMissingTasks(){
-  var lastColumn = tasksSheet.getLastColumn();
-  var lastRow = tasksSheet.getLastRow();
-  var allTasks = []
+      let body = 'You are missing the following tasks:\n' +
+                  tasks[i].map(task => `  - ${task}`).join('\n') + 
+                  '\n\nPlease check your Flight Tracker to get these completed.';
 
-  for (var col = 4; col <= lastColumn; col++) { // Change start col as a var
-    // Iterate through each row in the current column
-    //console.log("NEW RA");
-    var raName = tasksSheet.getRange(taskRow, col).getValue();
-    var taskList = [];
-    for (var row = 1; row <= lastRow; row++) {
-      // Get the value of the current cell
-      var cellValue = tasksSheet.getRange(row, col).getValue();
-      
-      // Check if the cell value is ❌
-      if (cellValue === '❌') {
-        // Get values from the taskRow and taskCol
-        var taskName = tasksSheet.getRange(row, taskCol).getValue();
-        taskList.push(taskName)
-      }
+      sendEmail(emailAddress, subject, body)
     }
-  console.log(raName)
-  console.log(taskList)
-  //taskDict[raName] = taskList
-  allTasks.push(taskList)
   }
-  console.log(allTasks)
-
 }
+
+function sendEmail(recipient, subject, body, cc=null){
+    var options = {};
+    if (cc !== null && cc.length > 0) {
+        options.cc = cc.join(',');
+    }
+    GmailApp.sendEmail(recipient, subject, body, options);    
+}
+
+// TODO - We don't need two loops.
+// Just send the email after getting list of tasks
