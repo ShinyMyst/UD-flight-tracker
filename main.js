f65f0a7784362917d597d80018711addcc3b859b
ShinyMyst <122401790+ShinyMyst@users.noreply.github.com>
2024-09-13 13:42:38 -0400

    Remidners, functional version.

--- main.js
+++ main.js
@@ -10,41 +10,36 @@
   var startCol = 4    // First col with an RA name
   var emailRow = 2    // Row with first email address
   var emailCol = 2    // Col holding email addresses
-
-  var subject = "[REMINDER] Missing Tasks"
-
-
-  var lastColumn = taskSheet.getLastColumn();
-
+  
+  var taskSubject = "[REMINDER] Missing Tasks"
+  var taskCC = ['email'];
 
 
 function main() {
-  // var missingTasks = getMissingTasks(taskSheet, taskNameCol, startCol)
-  // sendReminders(emailSheet, emailRow, emailCol, missingTasks);
   sendTaskReminder()
 }
 
-////////////
-// Functions
-////////////
+
 function sendTaskReminder(){
   // Iterate through each column (RA Names)
-  for (let col = startCol; col <= lastColumn; col++) { 
+  let lastTaskCol = taskSheet.getLastColumn();
+  for (let col = startCol; col <= lastTaskCol; col++) { 
     tasks = getTasks(col);
     // Prep and send email
     if (tasks.length > 0) {
-    emailAddress = emailSheet.getRange(col-startCol+emailRow, emailCol).getValue();
-    let body = 'You are missing the following tasks:\n' +
-            tasks.map(tasks => `  - ${tasks}`).join('\n') + 
-            '\n\nPlease check your Flight Tracker to get these completed.';
+      emailAddress = emailSheet.getRange(col-startCol+emailRow, emailCol).getValue();
+      let body = 'You are missing the following tasks:\n' +
+              tasks.map(tasks => `  - ${tasks}`).join('\n') + 
+              '\n\nPlease check your Flight Tracker to get these completed.';
 
-    console.log(emailAddress)
-    console.log(emailSheet.getRange(col-startCol+emailRow, emailCol-1).getValue())
-    console.log(body)
-    // sendEmail(emailAddress, subject, body)
+      //console.log(emailAddress)
+      //console.log(emailSheet.getRange(col-startCol+emailRow, emailCol-1).getValue())
+      //console.log(body)
+      sendEmail(emailAddress, taskSubject, body, taskCC)
     }
   }
-}
+};
+
 
 function getTasks(targetCol) {
   /* Returns a list of all tasks with an X in target column */
@@ -59,7 +54,7 @@ function getTasks(targetCol) {
     }
   }
   return tasks
-}
+};
 
 
 function sendEmail(recipient, subject, body, cc=null){
@@ -68,4 +63,4 @@ function sendEmail(recipient, subject, body, cc=null){
         options.cc = cc.join(',');
     }
     GmailApp.sendEmail(recipient, subject, body, options);    
-}
+};
